d94a19206bfcf159ff6b5f516a9b62c53e35e233
mehmet can yolar <mehmetcanyolar@gmail.com>
2024-11-11 19:32:36 +0300

    ef olarak Join işleminide içeren CRUD işlemleri yaıldı(movie + category merged by id and table restructured as consisting of category name rather than its category id

---
+++ FormMovie.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Project04_EntityFrameworkCodeFirstMovie
+{
+    partial class FormMovie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnList = new System.Windows.Forms.Button();
+            this.txtDuration = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.mskDate = new System.Windows.Forms.MaskedTextBox();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.btnMovieWithCategory = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(504, 151);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(116, 23);
+            this.btnSearch.TabIndex = 19;
+            this.btnSearch.Text = "Ara";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(504, 192);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(116, 23);
+            this.btnUpdate.TabIndex = 18;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(504, 115);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(116, 23);
+            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(504, 77);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(116, 23);
+            this.btnCreate.TabIndex = 16;
+            this.btnCreate.Text = "Ekle";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(194, 74);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(219, 22);
+            this.txtName.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(112, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Film Adı:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 285);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1109, 309);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(194, 43);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(219, 22);
+            this.txtId.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(122, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Film Id:";
+            // 
+            // btnList
+            // 
+            this.btnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnList.Location = new System.Drawing.Point(504, 39);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(116, 23);
+            this.btnList.TabIndex = 10;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // txtDuration
+            // 
+            this.txtDuration.Location = new System.Drawing.Point(194, 110);
+            this.txtDuration.Name = "txtDuration";
+            this.txtDuration.Size = new System.Drawing.Size(219, 22);
+            this.txtDuration.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(91, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 16);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Film Süresi:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(194, 148);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(219, 22);
+            this.txtDescription.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(104, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Açıklama:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(79, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 16);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "İzleme Tarihi:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(106, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 16);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Kategori :";
+            // 
+            // mskDate
+            // 
+            this.mskDate.Location = new System.Drawing.Point(194, 189);
+            this.mskDate.Mask = "00/00/0000";
+            this.mskDate.Name = "mskDate";
+            this.mskDate.Size = new System.Drawing.Size(219, 22);
+            this.mskDate.TabIndex = 28;
+            this.mskDate.ValidatingType = typeof(System.DateTime);
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(194, 224);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(219, 24);
+            this.cmbCategory.TabIndex = 29;
+            // 
+            // btnMovieWithCategory
+            // 
+            this.btnMovieWithCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMovieWithCategory.Location = new System.Drawing.Point(504, 221);
+            this.btnMovieWithCategory.Name = "btnMovieWithCategory";
+            this.btnMovieWithCategory.Size = new System.Drawing.Size(116, 58);
+            this.btnMovieWithCategory.TabIndex = 30;
+            this.btnMovieWithCategory.Text = "Kategoriyle FilmListele";
+            this.btnMovieWithCategory.UseVisualStyleBackColor = true;
+            this.btnMovieWithCategory.Click += new System.EventHandler(this.btnMovieWithCategory_Click);
+            // 
+            // FormMovie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1146, 606);
+            this.Controls.Add(this.btnMovieWithCategory);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.mskDate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDuration);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnList);
+            this.Name = "FormMovie";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormMovie";
+            this.Load += new System.EventHandler(this.FormMovie_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.TextBox txtDuration;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox mskDate;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Button btnMovieWithCategory;
+    }
+}
---
+++ FormMovie.cs
@@ -0,0 +1,105 @@
+﻿using Project04_EntityFrameworkCodeFirstMovie.DAL.Contex;
+using Project04_EntityFrameworkCodeFirstMovie.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project04_EntityFrameworkCodeFirstMovie
+{
+    public partial class FormMovie : Form
+    {
+        public FormMovie()
+        {
+            InitializeComponent();
+        }
+
+        MovieContext context = new MovieContext();  
+
+        void CategoryList()
+        {
+            var values = context.Categories.ToList();
+            cmbCategory.DisplayMember = "CategoryName";
+            cmbCategory.ValueMember = "CategoryId";
+            cmbCategory.DataSource = values;
+
+        }
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = context.Movies.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void FormMovie_Load(object sender, EventArgs e)
+        {
+            CategoryList(); 
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            Movie movie = new Movie();
+
+            movie.MovieTitle = txtName.Text;
+            movie.Duration= int.Parse(txtDuration.Text);
+            movie.Description = txtDescription.Text;
+            movie.CreateDate = DateTime.Parse(mskDate.Text);   //DateTime formatı maskbox ta US sistemine göre
+            movie.CategoryId = int.Parse(cmbCategory.SelectedValue.ToString());
+            context.Movies.Add(movie);
+            context.SaveChanges();
+            MessageBox.Show("İşlem Başarılı");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+               var value = context.Movies.Find(id);
+             
+           
+
+            context.Movies.Remove(value);
+
+            context.SaveChanges();
+            MessageBox.Show("İşlem Başarılı");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var values = context.Movies.Where(x=>x.MovieTitle==txtName.Text).ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var value = context.Movies.Find(id);
+            value.MovieTitle = txtName.Text;
+            value.Duration = int.Parse(txtDuration.Text);
+            value.Description = txtDescription.Text;
+            value.CreateDate = DateTime.Parse(mskDate.Text);   //DateTime formatı maskbox ta US sistemine göre bilgisayardan alıyor sistem özellikle datetime ise ondan benim pc us formata göre
+            value.CategoryId = int.Parse(cmbCategory.SelectedValue.ToString());
+
+
+            context.SaveChanges();
+            MessageBox.Show("İşlem Başarılı");
+        }
+
+        private void btnMovieWithCategory_Click(object sender, EventArgs e)
+        {
+            var values = context.Movies.Join(context.Categories,movies =>movies.CategoryId,category =>category.CategoryId,(movie,category)=> new
+            {
+                MovieId = movie.MovieId,
+                MovieTitle=movie.MovieTitle,
+                Description=movie.Description,
+                Duration=movie.Duration,
+                CategoryName=category.CategoryName
+            }).ToList();
+
+            dataGridView1.DataSource = values;
+        }
+    }
+}
